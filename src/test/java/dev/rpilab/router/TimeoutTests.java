dbac06f78fb184ed3a8c654aaecbb0220ef021cc
Alexandre Roman <alexandre.roman@broadcom.com>
2025-02-05 09:19:06 +0100

    Set read timeout to 60s

---
+++ src/test/java/dev/rpilab/router/TimeoutTests.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025 Broadcom, Inc. or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.rpilab.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"spring.cloud.gateway.httpclient.connect-timeout=1000",
+                "spring.cloud.gateway.httpclient.response-timeout=1000"})
+@AutoConfigureWireMock(port = 0)
+@ActiveProfiles("test")
+class TimeoutTests {
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void testTimeout() {
+        stubFor(get("/").willReturn(aResponse().withFixedDelay(10 * 1000).withStatus(200).withBody("Timeout")));
+        client.get().uri("/").header(HttpHeaders.HOST, "hello.corp.com").exchange()
+                .expectStatus().isEqualTo(HttpStatus.GATEWAY_TIMEOUT);
+    }
+}
